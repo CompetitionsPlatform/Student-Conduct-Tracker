ad59181c640252e570e2324c0fe1b3ec8e14a20f
Ronel Ramtahal <94023959+g0dbot@users.noreply.github.com>
2023-12-03 03:35:02 -0400

    Update index.py

--- App/views/index.py
+++ App/views/index.py
@@ -17,14 +17,29 @@ def index_page():
 def generate_random_contact_number():
     return f"0000-{random.randint(100, 999)}-{random.randint(1000, 9999)}"
 
-
 @index_views.route('/init', methods=['GET'])
 def init():
     # Assuming db is your SQLAlchemy database object
     db.drop_all()
     db.create_all()
 
-    admin = create_user('bob', 'boblast', 'bobpass')
+    admin = Admin('bob', 'boblast', 'bobpass')
+
+    if admin:
+        db.session.add(admin)
+        db.session.commit()
+    else:
+        return jsonify({'error':'database error'}), 201
+
+    log_staff = create_staff(admin, 'staff', 'stafflast', 'staffpass', '99', 'staff@schooling.com', 10)
+    log_staff2 = create_staff(admin, 'nice', 'nicelast', 'nicepass', '69', 'staff2@schooling.com', 9)
+
+    if log_staff and log_staff2:
+        db.session.add(log_staff)
+        db.session.add(log_staff2)
+        db.session.commit()
+    else:
+        return jsonify({'error':'database error'}), 201
 
     for ID in range(2, 50):
         staff = create_staff(admin,
@@ -38,7 +53,7 @@ def init():
         db.session.add(staff)
 
     db.session.commit()
-
+    
     for ID in range(50, 150):
         contact = generate_random_contact_number()
         student = create_student(admin, str(ID),
@@ -53,22 +68,14 @@ def init():
 
     db.session.commit()
 
-    # Log in the admin user
-    user = login(admin.ID, 'bobpass')
+    log_stu = create_student(admin, '813', 'alice', 'alicelast', 'alicelast', contact, random.choice(['Full-Time', 'Part-Time', 'Evening']), str(random.randint(1, 8)))
+    log_stu2 = create_student(admin, '666', 'trudy', 'trudylast', 'trudylast', contact, random.choice(['Full-Time', 'Part-Time', 'Evening']), str(random.randint(1, 8)))
 
-    if user:
-        # Log in the user
-        session['logged_in'] = True
-
-        # Create a JWT token
-        token = create_access_token(identity=user.ID)
-
-        # Return the token as part of the response
-        response = {'message': 'Admin user logged in', 'jwt_token': token}
+    if log_stu and log_stu2:
+        db.session.add(log_stu)
+        db.session.add(log_stu2)
+        db.session.commit()
     else:
-        response = {'message': 'Admin login failed'}
+        return jsonify({'error':'database error'}), 201
 
-    # Optionally, print the response or return it
-    return jsonify(response), 201
-
-    #return jsonify({'message': 'Database initialized'},{f'token':'{token}'}), 201
+    return jsonify({'message':'database initialized'}), 201
